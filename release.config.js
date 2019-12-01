044b5b30a767503195ac458fa6444b3ad9fde5b8
Jeff <sagalbot@gmail.com>
2019-11-30 20:16:08 -0800

    ci(semantic-release): add NPM plugin

--- release.config.js
+++ release.config.js
@@ -2,12 +2,14 @@ module.exports = {
   'release': {
     'branch': 'master',
   },
-  "plugins": [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    ["@semantic-release/git", {
-      "assets": ["package.json"],
-      "message": "chore(🚀): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }]
-  ]
+  'plugins': [
+    '@semantic-release/npm',
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/git', {
+      'assets': ['package.json'],
+      'message': 'chore(🚀): ${nextRelease.version}',
+    }],
+  ],
 };
